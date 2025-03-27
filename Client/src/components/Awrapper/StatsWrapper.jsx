9cd34df52cb420da201208dc5d5a4f053e7a1f12
Amit Kumar Sahani <121219168+amit-23-hub@users.noreply.github.com>
2025-03-27 13:42:18 +0530

    work on responsive

--- Client/src/components/Awrapper/StatsWrapper.jsx
+++ Client/src/components/Awrapper/StatsWrapper.jsx
@@ -1,38 +1,28 @@
 import React from 'react';
-import styles from './StatsWrapper.module.css'; 
+import styles from './StatsWrapper.module.css';
 
 const StatsWrapper = () => {
   return (
     <div className={styles.statsWrapper}>
       <div className={styles.statContainer}>
-       
         <div className={styles.statItem}>
-          <div className={styles.statValue}>50M</div>
-          <div className={styles.statDescription}>Interviews completed</div>
+          <span className={styles.statValue}>50M</span>
+          <span className={styles.statDescription}>Interviews completed</span>
         </div>
 
-        <div className={styles.verticalLine}></div>
-
-        
         <div className={styles.statItem}>
-          <div className={styles.statValue}>80%</div>
-          <div className={styles.statDescription}>Decrease in time to hire</div>
+          <span className={styles.statValue}>80%</span>
+          <span className={styles.statDescription}>Decrease in time to hire</span>
         </div>
 
-        <div className={styles.verticalLine}></div>
-
-    
         <div className={styles.statItem}>
-          <div className={styles.statValue}>50%</div>
-          <div className={styles.statDescription}>Decrease cost per interview</div>
+          <span className={styles.statValue}>50%</span>
+          <span className={styles.statDescription}>Decrease cost per interview</span>
         </div>
 
-        <div className={styles.verticalLine}></div>
-
-        
         <div className={styles.statItem}>
-          <div className={styles.statValue}>16%</div>
-          <div className={styles.statDescription}>Increase in DEI hiring goals</div>
+          <span className={styles.statValue}>16%</span>
+          <span className={styles.statDescription}>Increase in DEI hiring goals</span>
         </div>
       </div>
     </div>
